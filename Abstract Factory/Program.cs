c97c943607e52ae00767a992d43f770bc025db11
Дмитрий Копейкин <kapew2284@gmail.com>
2023-02-09 14:56:09 +0500

    Create Abstract Factory

---
+++ Abstract Factory/Program.cs
@@ -0,0 +1,175 @@
+﻿namespace Abstract_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero elf = new Hero(new ElfFactory());
+            elf.Hit();
+            elf.Run();
+
+            Hero voin = new Hero(new VoinFactory());
+            voin.Hit();
+            voin.Run();
+        }
+    }
+    //абстрактный класс - оружие
+    abstract class Weapon
+    {
+        public abstract void Hit();
+    }
+    // абстрактный класс движение
+    abstract class Movement
+    {
+        public abstract void Move();
+    }
+
+    // класс арбалет
+    class Arbalet : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Стреляем из арбалета");
+        }
+    }
+    // класс меч
+    class Sword : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Бьем мечом");
+        }
+    }
+    // движение полета
+    class FlyMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Летим");
+        }
+    }
+    // движение - бег
+    class RunMovement : Movement
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Бежим");
+        }
+    }
+    // класс абстрактной фабрики
+    abstract class HeroFactory
+    {
+        public abstract Movement CreateMovement();
+        public abstract Weapon CreateWeapon();
+    }
+    // Фабрика создания летящего героя с арбалетом
+    class ElfFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new FlyMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Arbalet();
+        }
+    }
+    // Фабрика создания бегущего героя с мечом
+    class VoinFactory : HeroFactory
+    {
+        public override Movement CreateMovement()
+        {
+            return new RunMovement();
+        }
+
+        public override Weapon CreateWeapon()
+        {
+            return new Sword();
+        }
+    }
+    // клиент - сам супергерой
+    class Hero
+    {
+        private Weapon weapon;
+        private Movement movement;
+        public Hero(HeroFactory factory)
+        {
+            weapon = factory.CreateWeapon();
+            movement = factory.CreateMovement();
+        }
+        public void Run()
+        {
+            movement.Move();
+        }
+        public void Hit()
+        {
+            weapon.Hit();
+        }
+    }
+
+
+    #region Concept
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+
+    abstract class AbstractProductA
+    { }
+
+    abstract class AbstractProductB
+    { }
+
+    class ProductA1 : AbstractProductA
+    { }
+
+    class ProductB1 : AbstractProductB
+    { }
+
+    class ProductA2 : AbstractProductA
+    { }
+
+    class ProductB2 : AbstractProductB
+    { }
+
+    class Client
+    {
+        private AbstractProductA abstractProductA;
+        private AbstractProductB abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            abstractProductB = factory.CreateProductB();
+            abstractProductA = factory.CreateProductA();
+        }
+        public void Run()
+        { }
+    }
+    #endregion
+}
