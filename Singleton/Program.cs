2a6328afb3ac5c01a4c8d6f4e17ce1f050b3fe06
Дмитрий Копейкин <kapew2284@gmail.com>
2023-02-09 15:19:51 +0500

    Create Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            comp.Launch("Windows 8.1");
+            Console.WriteLine(comp.OS.Name);
+
+            // у нас не получится изменить ОС, так как объект уже создан    
+            comp.OS = OS.getInstance("Windows 10");
+            Console.WriteLine(comp.OS.Name);
+        }
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.getInstance(osName);
+        }
+    }
+    class OS
+    {
+        private static OS instance;
+
+        public string Name { get; private set; }
+
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+                instance = new OS(name);
+            return instance;
+        }
+    }
+
+
+    #region Concept
+    class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+    #endregion
+}
